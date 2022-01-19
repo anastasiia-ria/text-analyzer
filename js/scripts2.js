e1d96916003f9aa5e21e7f20a2fec6246fd51e68
Anastasiia Riabets <anastasiia.ria@gmail.com>
2022-01-19 14:47:47 -0800

    test2 completed for pigLatin
    Co-Authored-by: Hayley M <hayleymcvay8@gmail.com>

--- js/scripts2.js
+++ js/scripts2.js
@@ -2,13 +2,19 @@
 function pigLatin(word) {
   const array = word.split('');
   const vowels = ["a", "e", "i", "o", "u"];
+  let consonant = true;
 
   vowels.forEach(function(vowel) {
     if (array[0] === vowel) {
       array.push("way");
-    }
+      consonant = false;
+    } 
   });
-  
+
+  if (consonant) {
+    array.push(array.shift());
+  }
+
   word = array.join("");
   console.log(word);
 }
